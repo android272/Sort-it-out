e18a10965b8b0e746c89bde62e2bc68a32bdc42a
Apullins <android2772@gmail.com>
2019-05-20 19:52:22 -0400

    Wrote test to check that array of 2 gets sorted.

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,5 +1,10 @@
 public class BubbleSort {
     public static int[] sort(int[] ints) {
+        if(ints[0] > ints[1]) {
+            int temp = ints[1];
+            ints[1] = ints[0];
+            ints[0] = temp;
+        }
         return ints;
     }
 }
--- test/BubbleSortTest.java
+++ test/BubbleSortTest.java
@@ -4,6 +4,11 @@ import static org.junit.Assert.*;
 public class BubbleSortTest {
     @Test
     public void When_Bubble_Sort_Is_Passed_An_Array_Of_Sorted_Numbers_It_Returns_A_Sorted_Array() {
-        assertArrayEquals(new int[]{1, 2, 3}, BubbleSort.sort(new int[] {1, 2, 3}));
+        assertArrayEquals(new int[]{1, 2}, BubbleSort.sort(new int[] {1, 2}));
+    }
+
+    @Test
+    public void When_Bubble_Sort_Is_Passed_An_Array_Of_Unsorted_Numbers_It_Returns_A_Sorted_Array() {
+        assertArrayEquals(new int[]{1, 2}, BubbleSort.sort(new int[] {2, 1}));
     }
 }
