b9f120f6ec1ad4c9a24d9c389cec9276e7b44e8a
Apullins <android2772@gmail.com>
2019-05-21 15:23:44 -0400

    Bubble Sort Class is now finished. You can sort ascending and descending.

--- test/BubbleSortTest.java
+++ test/BubbleSortTest.java
@@ -12,8 +12,8 @@ public class BubbleSortTest {
     @Test
     public void When_Bubble_Sort_Is_Passed_An_Unsorted_Array_It_Returns_An_Ascending_Sorted_Array() {
         assertArrayEquals(new int[]{1, 2}, BubbleSort.sortAscending(new int[] {2, 1}));
-        assertArrayEquals(new int[]{2, 18, 30}, BubbleSort.sortAscending(new int[] {30, 18, 2}));
-        assertArrayEquals(new int[]{15, 22, 36, 44}, BubbleSort.sortAscending(new int[] {36, 44, 15, 22}));
+        assertArrayEquals(new int[]{1, 2, 3}, BubbleSort.sortAscending(new int[] {3, 1, 2}));
+        assertArrayEquals(new int[]{1, 2, 3, 4}, BubbleSort.sortAscending(new int[] {3, 4, 1, 2}));
     }
 
     @Test
@@ -26,16 +26,7 @@ public class BubbleSortTest {
     @Test
     public void When_Bubble_Sort_Is_Passed_An_Unsorted_Array_It_Returns_An_Descending_Sorted_Array() {
         assertArrayEquals(new int[]{2, 1}, BubbleSort.sortDescending(new int[] {1, 2}));
-    }
-
-    @Test
-    public void When_Bubble_Sort_Is_Passed_An_Unsorted_Array_Of_Length_Three_It_Returns_An_Descending_Sorted_Array() {
         assertArrayEquals(new int[]{3, 2, 1}, BubbleSort.sortDescending(new int[] {1, 3, 2}));
-    }
-
-    @Test
-    public void When_Bubble_Sort_Is_Passed_An_Unsorted_Array_Of_Length_Four_It_Returns_An_Descending_Sorted_Array() {
         assertArrayEquals(new int[]{4, 3, 2, 1}, BubbleSort.sortDescending(new int[] {1, 3, 2, 4}));
-        assertArrayEquals(new int[]{4, 3, 2, 1}, BubbleSort.sortDescending(new int[] {3, 1, 2, 4}));
     }
 }
