6021459f5838ff15b3e485f0045fc2e8f44ed1a6
Apullins <android2772@gmail.com>
2019-05-21 17:39:03 -0400

    Discovered a way to optimize bubble sort so I implemented this improvement. I left the old code the way it was so that I could test how much more efficient this optimization really is.

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -12,6 +12,24 @@ public class BubbleSort {
         return ints;
     }
 
+    public static int[] sortAscendingOptimized(int[] ints) {
+        boolean swapped;
+        for(int i = 0; i < ints.length - 1; i++) {
+            swapped = false;
+            for(int j = 0; j < ints.length - i - 1; j++) {
+                if (ints[j] > ints[j + 1]) {
+                    int temp = ints[j];
+                    ints[j] = ints[j + 1];
+                    ints[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if(swapped == false) { break; }
+        }
+        return ints;
+    }
+
     public static int[] sortDescending(int[] ints) {
         for(int i = 0; i < ints.length - 1; i++) {
             for (int j = 0; j < ints.length - i - 1; j++) {
--- test/BubbleSortTest.java
+++ test/BubbleSortTest.java
@@ -29,4 +29,18 @@ public class BubbleSortTest {
         assertArrayEquals(new int[]{3, 2, 1}, BubbleSort.sortDescending(new int[] {1, 3, 2}));
         assertArrayEquals(new int[]{4, 3, 2, 1}, BubbleSort.sortDescending(new int[] {1, 3, 2, 4}));
     }
+
+    @Test
+    public void When_Optimized_Bubble_Sort_Is_Passed_A_Sorted_Array_It_Returns_An_Ascending_Sorted_Array() {
+        assertArrayEquals(new int[]{1, 2}, BubbleSort.sortAscendingOptimized(new int[] {1, 2}));
+        assertArrayEquals(new int[]{1, 2, 3}, BubbleSort.sortAscendingOptimized(new int[] {1, 2, 3}));
+        assertArrayEquals(new int[]{1, 2, 3, 4}, BubbleSort.sortAscendingOptimized(new int[] {1, 2, 3, 4}));
+    }
+
+    @Test
+    public void When_Optimized_Bubble_Sort_Is_Passed_An_Unsorted_Array_It_Returns_An_Ascending_Sorted_Array() {
+        assertArrayEquals(new int[]{1, 2}, BubbleSort.sortAscendingOptimized(new int[] {2, 1}));
+        assertArrayEquals(new int[]{1, 2, 3}, BubbleSort.sortAscendingOptimized(new int[] {3, 1, 2}));
+        assertArrayEquals(new int[]{1, 2, 3, 4}, BubbleSort.sortAscendingOptimized(new int[] {3, 4, 1, 2}));
+    }
 }
