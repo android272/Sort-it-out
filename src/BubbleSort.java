39f6154b1f9eb9bd041292a900abd7adba5e46f6
Apullins <android2772@gmail.com>
2019-05-21 17:41:56 -0400

    Optimized the descending bubble sort.

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -42,4 +42,22 @@ public class BubbleSort {
         }
         return ints;
     }
+
+    public static int[] sortDescendingOptimized(int[] ints) {
+        boolean swapped;
+        for(int i = 0; i < ints.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < ints.length - i - 1; j++) {
+                if (ints[j] < ints[j + 1]) {
+                    int temp = ints[j];
+                    ints[j] = ints[j + 1];
+                    ints[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if(swapped == false) { break; }
+        }
+        return ints;
+    }
 }
--- test/BubbleSortTest.java
+++ test/BubbleSortTest.java
@@ -43,4 +43,18 @@ public class BubbleSortTest {
         assertArrayEquals(new int[]{1, 2, 3}, BubbleSort.sortAscendingOptimized(new int[] {3, 1, 2}));
         assertArrayEquals(new int[]{1, 2, 3, 4}, BubbleSort.sortAscendingOptimized(new int[] {3, 4, 1, 2}));
     }
+
+    @Test
+    public void When_Optimized_Bubble_Sort_Is_Passed_A_Sorted_Array_It_Returns_An_Descending_Sorted_Array() {
+        assertArrayEquals(new int[]{2, 1}, BubbleSort.sortDescendingOptimized(new int[] {2, 1}));
+        assertArrayEquals(new int[]{3, 2, 1}, BubbleSort.sortDescendingOptimized(new int[] {3, 2, 1}));
+        assertArrayEquals(new int[]{4, 3, 2, 1}, BubbleSort.sortDescendingOptimized(new int[] {4, 3, 2, 1}));
+    }
+
+    @Test
+    public void When_Optimized_Bubble_Sort_Is_Passed_An_Unsorted_Array_It_Returns_An_Descending_Sorted_Array() {
+        assertArrayEquals(new int[]{2, 1}, BubbleSort.sortDescendingOptimized(new int[] {1, 2}));
+        assertArrayEquals(new int[]{3, 2, 1}, BubbleSort.sortDescendingOptimized(new int[] {1, 3, 2}));
+        assertArrayEquals(new int[]{4, 3, 2, 1}, BubbleSort.sortDescendingOptimized(new int[] {1, 3, 2, 4}));
+    }
 }
