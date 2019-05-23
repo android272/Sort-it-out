f01367ff7b78daef971b64d6349e761a63babbd2
Apullins <android2772@gmail.com>
2019-05-23 14:53:03 -0400

    wrote test to find the smallest index in the array that will be passed to SelectionSort.sort.

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -2,4 +2,19 @@ public class SelectionSort {
     public static int[] Sort(int[] ints) {
         return ints;
     }
+
+    public static int findSmallestIndex(int[] ints) {
+        int temp;
+        for(int i = 0; i < ints.length; i++) {
+            for(int j = i + 1; j < ints.length; j++) {
+                if(ints[j] < ints[i]) {
+                    temp = ints[i];
+                    ints[i] = ints[j];
+                    ints[j] = temp;
+                }
+            }
+
+        }
+        return ints[0];
+    }
 }
--- test/SelectionSortTest.java
+++ test/SelectionSortTest.java
@@ -8,4 +8,11 @@ public class SelectionSortTest {
         assertArrayEquals(new int[] { 1, 2, 3 }, SelectionSort.Sort(new int[] { 1, 2, 3 }));
         assertArrayEquals(new int[] { 1, 2, 3, 4 }, SelectionSort.Sort(new int[] { 1, 2, 3, 4 }));
     }
+
+    @Test
+    public void When_Find_Smallest_Index_Is_Passed_An_Array_It_Returns_The_Smallest_Index() {
+        assertEquals(1, SelectionSort.findSmallestIndex(new int[] { 2, 1 }));
+        assertEquals(1, SelectionSort.findSmallestIndex(new int[] { 3, 2, 1 }));
+        assertEquals(1, SelectionSort.findSmallestIndex(new int[] { 4, 3, 2, 1 }));
+    }
 }
