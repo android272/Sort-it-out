0c6c211f4c38ab3bf8e10d87f11094c8fcc8e24d
Apullins <android2772@gmail.com>
2019-05-24 17:48:23 -0400

    Finished Selection Sort.

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,21 +1,25 @@
 public class SelectionSort {
     public static int[] Sort(int[] ints) {
+        for(int i = 0; i < ints.length; i++) {
+            int index = findSmallestIndex(ints, i);
+            ints = swap(ints, i, index);
+        }
+
         return ints;
     }
 
-    public static int findSmallestIndex(int[] ints) {
-        int temp;
-        for(int i = 0; i < ints.length; i++) {
-            for(int j = i + 1; j < ints.length; j++) {
-                if(ints[j] < ints[i]) {
-                    temp = ints[i];
-                    ints[i] = ints[j];
-                    ints[j] = temp;
-                }
-            }
+    public static int findSmallestIndex(int[] ints, int startIndex) {
+        int minValue = ints[startIndex];
+        int minIndex = startIndex;
 
+        for(int i = minIndex + 1; i < ints.length; i++) {
+            if(ints[i] < minValue) {
+                minIndex = i;
+                minValue = ints[i];
+            }
         }
-        return ints[0];
+
+        return minIndex;
     }
 
     public static int[] swap(int[] ints, int firstIndex, int secondIndex) {
--- test/SelectionSortTest.java
+++ test/SelectionSortTest.java
@@ -10,16 +10,16 @@ public class SelectionSortTest {
     }
 
     @Test
-    public void When_Find_Smallest_Index_Is_Passed_An_Array_It_Returns_The_Smallest_Index() {
-        assertEquals(1, SelectionSort.findSmallestIndex(new int[] { 2, 1 }));
-        assertEquals(1, SelectionSort.findSmallestIndex(new int[] { 3, 2, 1 }));
-        assertEquals(1, SelectionSort.findSmallestIndex(new int[] { 4, 3, 2, 1 }));
-    }
-
-    @Test
     public void When_Swap_Is_Passed_Two_Indicies_Of_An_Array_Swap_Them() {
         assertArrayEquals(new int[] { 1, 2, 5, 8 }, SelectionSort.swap(new int[] { 8, 2, 5, 1 }, 0, 3));
         assertArrayEquals(new int[] { 8, 5, 2, 1 }, SelectionSort.swap(new int[] { 8, 2, 5, 1 }, 1, 2));
         assertArrayEquals(new int[] { 8, 1, 5, 2 }, SelectionSort.swap(new int[] { 8, 2, 5, 1 }, 1, 3));
+    }
+
+    @Test
+    public void When_Selection_Sort_Is_Passed_An_Unsorted_Array_It_Returns_An_Ascending_Sorted_Array() {
+        assertArrayEquals(new int[] { 1, 2 }, SelectionSort.Sort(new int[] { 2, 1 }));
+        assertArrayEquals(new int[] { 1, 2, 3 }, SelectionSort.Sort(new int[] { 2, 3, 1 }));
+        assertArrayEquals(new int[] { 1, 2, 3, 4 }, SelectionSort.Sort(new int[] { 4, 3, 1, 2 }));
     }
 }
