5c247817e71843ea4a1e771a8a7bfbc7b17c60cc
Apullins <android2772@gmail.com>
2019-06-06 17:26:43 -0400

    Found out that the way I thought the Insertion Sort worked was wrong. So I rewrote the first test and added code to make it pass.

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,10 +1,5 @@
 public class InsertionSort {
-    public static int[] Sort(int[] ints, int insertion) {
-        int[] output = new int[ints.length + 1];
-        for (int i = 0; i < ints.length; i++) {
-            output[i] = ints[i];
-        }
-        output[output.length - 1] = insertion;
-        return output;
+    public static int[] Sort(int[] ints) {
+        return ints;
     }
 }
--- test/InsertionSortTest.java
+++ test/InsertionSortTest.java
@@ -3,9 +3,9 @@ import static org.junit.Assert.*;
 
 public class InsertionSortTest {
     @Test
-    public void When_Insertion_Sort_Is_Passed_An_Array_And_A_Number_A_Sorted_Array_Is_Returned() {
-        assertArrayEquals(new int[] { 1, 2, 3 }, InsertionSort.Sort(new int[] { 1, 2 }, 3));
-        assertArrayEquals(new int[] { 1, 2, 3, 4 }, InsertionSort.Sort(new int[] { 1, 2, 3 }, 4));
-        assertArrayEquals(new int[] { 1, 2, 3, 4, 5 }, InsertionSort.Sort(new int[] { 1, 2, 3, 4 }, 5));
+    public void When_Insertion_Sort_Is_Passed_A_Sorted_Array_It_Returns_A_Sorted_Array() {
+        assertArrayEquals(new int[] { 1, 2 }, InsertionSort.Sort(new int[] { 1, 2 }));
+        assertArrayEquals(new int[] { 1, 2, 3 }, InsertionSort.Sort(new int[] { 1, 2, 3 }));
+        assertArrayEquals(new int[] { 1, 2, 3, 4 }, InsertionSort.Sort(new int[] { 1, 2, 3, 4 }));
     }
 }
