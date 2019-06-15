ce79a59b07f9fddbf01ecf1d7008314159702d83
Apullins <android2772@gmail.com>
2019-06-15 17:40:01 -0400

    Test that two values are reordered as needed. Test passes.

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,5 +1,14 @@
 public class MergeSort {
     public static int[] Sort(int[] ints) {
+        if(ints[0] > ints[1]) {
+            int tmp = ints[0];
+            ints[0] = ints[1];
+            ints[1] = tmp;
+        }
         return ints;
+    }
+
+    private static void merge(int[] input, int start, int half, int end) {
+
     }
 }
--- test/MergeSortTest.java
+++ test/MergeSortTest.java
@@ -8,4 +8,9 @@ public class MergeSortTest {
         assertArrayEquals(new int[] { 1, 2, 3 }, MergeSort.Sort(new int[] { 1, 2, 3 }));
         assertArrayEquals(new int[] { 1, 2, 3, 4 }, MergeSort.Sort(new int[] { 1, 2, 3, 4 }));
     }
+
+    @Test
+    public void When_Merge_Sort_Is_Passed_An_Unsorted_Array_It_Returns_A_Sorted_Array() {
+        assertArrayEquals(new int[] { 1, 2 }, MergeSort.Sort(new int[] { 2, 1 }));
+    }
 }
