f42a5774d25a29d673e326276dd51d79ba04efec
Apullins <android2772@gmail.com>
2019-06-15 19:03:17 -0400

    Test that two values are reordered as needed. Test passes.

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,9 +1,55 @@
 public class MergeSort {
     public static int[] Sort(int[] ints) {
-        if(ints[0] > ints[1]) {
-            int tmp = ints[0];
-            ints[0] = ints[1];
-            ints[1] = tmp;
+        if(ints.length == 2) {
+            if (ints[0] > ints[1]) {
+                int tmp = ints[0];
+                ints[0] = ints[1];
+                ints[1] = tmp;
+            }
+        } else {
+            int start = 0;
+            int end = ints.length - 1;
+            int midPoint = (start + end) /2;
+
+            if (ints[start] > ints[midPoint]) {
+                int tmp = ints[start];
+                ints[start] = ints[midPoint];
+                ints[midPoint] = tmp;
+            }
+
+            int[] workspace = new int[end - start + 1];
+
+            int workspaceIndex = 0;
+            int firstIndex = start;
+            int secondIndex = midPoint + 1;
+
+            while (workspaceIndex < workspace.length) {
+                if(secondIndex <= end && firstIndex <= midPoint) {
+                    if (ints[firstIndex] > ints[secondIndex]) {
+                        workspace[workspaceIndex] = ints[secondIndex];
+                        workspaceIndex++;
+                        secondIndex++;
+                    } else {
+                        workspace[workspaceIndex] = ints[firstIndex];
+                        workspaceIndex++;
+                        firstIndex++;
+                    }
+                } else if(firstIndex > midPoint){
+                    workspace[workspaceIndex] = ints[secondIndex];
+                    workspaceIndex++;
+                    secondIndex++;
+                } else {
+                    workspace[workspaceIndex] = ints[firstIndex];
+                    workspaceIndex++;
+                    firstIndex++;
+                }
+            }
+
+            for(int i = 0; i < workspace.length; i++) {
+                System.out.print(workspace[i]);
+                ints[i] = workspace[i];
+            }
+            System.out.println();
         }
         return ints;
     }
--- test/MergeSortTest.java
+++ test/MergeSortTest.java
@@ -10,7 +10,12 @@ public class MergeSortTest {
     }
 
     @Test
-    public void When_Merge_Sort_Is_Passed_An_Unsorted_Array_It_Returns_A_Sorted_Array() {
+    public void When_Merge_Sort_Is_Passed_An_Unsorted_Two_Element_Array_It_Returns_A_Sorted_Array() {
         assertArrayEquals(new int[] { 1, 2 }, MergeSort.Sort(new int[] { 2, 1 }));
+    }
+
+    @Test
+    public void When_Merge_Sort_Is_Passed_An_Array_Of_Three_Element_Array_It_Returns_Sorted() {
+        assertArrayEquals(new int[] { 1, 2, 3 }, MergeSort.Sort(new int[] { 3, 1, 2}));
     }
 }
