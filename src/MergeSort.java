8d428b5240408d44c8ecc1515447dcee85a7e48e
Apullins <android2772@gmail.com>
2019-06-28 18:45:42 -0400

    Refactored naming for clarity, now using recursion to divide the arrays and wrote a merge method. Test for array of length 5.

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,60 +1,53 @@
 public class MergeSort {
     public static int[] Sort(int[] ints) {
-        if(ints.length == 2) {
-            if (ints[0] > ints[1]) {
-                int tmp = ints[0];
-                ints[0] = ints[1];
-                ints[1] = tmp;
-            }
-        } else {
-            int start = 0;
-            int end = ints.length - 1;
-            int midPoint = (start + end) /2;
+        divideArray(ints, 0, ints.length - 1);
 
-            if (ints[start] > ints[midPoint]) {
-                int tmp = ints[start];
-                ints[start] = ints[midPoint];
-                ints[midPoint] = tmp;
-            }
-
-            int[] workspace = new int[end - start + 1];
-
-            int workspaceIndex = 0;
-            int firstIndex = start;
-            int secondIndex = midPoint + 1;
-
-            while (workspaceIndex < workspace.length) {
-                if(secondIndex <= end && firstIndex <= midPoint) {
-                    if (ints[firstIndex] > ints[secondIndex]) {
-                        workspace[workspaceIndex] = ints[secondIndex];
-                        workspaceIndex++;
-                        secondIndex++;
-                    } else {
-                        workspace[workspaceIndex] = ints[firstIndex];
-                        workspaceIndex++;
-                        firstIndex++;
-                    }
-                } else if(firstIndex > midPoint){
-                    workspace[workspaceIndex] = ints[secondIndex];
-                    workspaceIndex++;
-                    secondIndex++;
-                } else {
-                    workspace[workspaceIndex] = ints[firstIndex];
-                    workspaceIndex++;
-                    firstIndex++;
-                }
-            }
-
-            for(int i = 0; i < workspace.length; i++) {
-                System.out.print(workspace[i]);
-                ints[i] = workspace[i];
-            }
-            System.out.println();
-        }
         return ints;
     }
 
-    private static void merge(int[] input, int start, int half, int end) {
+    private static void divideArray(int[] ints, int start, int end) {
+        if(start < end) {
+            int midPoint = (start + end) / 2;
+
+            divideArray(ints, start, midPoint);
+            divideArray(ints, midPoint +1 , end);
+            merge(ints, start, midPoint, end);
+        }
+    }
+
+        //1, 3 and 2
+    private static void merge(int[] data, int start, int midPoint, int end) {
+        int workspaceLength = end - start + 1;
+        int[] workspace = new int[workspaceLength];
+        int workspaceIndex = 0;
+        int dataStart = start;
+        int dataEnd = midPoint + 1;
+
+        while (dataStart <= midPoint && dataEnd <= end) {
+            if (data[dataStart] < data[dataEnd]) {
+                workspace[workspaceIndex] = data[dataStart];
+                workspaceIndex++;
+                dataStart++;
+            } else {
+                workspace[workspaceIndex] = data[dataEnd];
+                workspaceIndex++;
+                dataEnd++;
+            }
+        }
+
+        while (dataStart <= midPoint) {
+            workspace[workspaceIndex] = data[dataStart];
+            workspaceIndex++;
+            dataStart++;
+        }
+
+        while (dataEnd <= end) {
+            workspace[workspaceIndex] = data[dataEnd];
+            workspaceIndex++;
+            dataEnd++;
+        }
+
+        System.arraycopy(workspace, 0, data, start, workspaceLength);
 
     }
 }
--- test/MergeSortTest.java
+++ test/MergeSortTest.java
@@ -15,7 +15,12 @@ public class MergeSortTest {
     }
 
     @Test
-    public void When_Merge_Sort_Is_Passed_An_Array_Of_Three_Element_Array_It_Returns_Sorted() {
+    public void When_Merge_Sort_Is_Passed_A_Three_Element_Array_It_Returns_Sorted() {
         assertArrayEquals(new int[] { 1, 2, 3 }, MergeSort.Sort(new int[] { 3, 1, 2}));
+    }
+
+    @Test
+    public  void When_Merge_Sort_Is_Passed_A_Five_Element_Array_It_Returns_Sorted() {
+        assertArrayEquals(new int[] { 1, 2, 3, 4, 5 }, MergeSort.Sort(new int[] { 4, 5, 3, 1, 2}));
     }
 }
