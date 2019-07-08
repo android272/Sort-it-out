7465900f208edc9b2f2758fb771f6a5adf84d084
Apullins <android2772@gmail.com>
2019-07-08 16:26:29 -0400

    Wrote test for when quick sort is passed an unsorted two element array.

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,5 +1,10 @@
 public class QuickSort {
     public static int[] Sort(int[] ints) {
+        if(ints[0] > ints[1]) {
+            int temp = ints[0];
+            ints[0] = ints[1];
+            ints[1] = temp;
+        }
         return ints;
     }
 }
--- test/QuickSortTest.java
+++ test/QuickSortTest.java
@@ -8,4 +8,9 @@ public class QuickSortTest {
         assertArrayEquals(new int[] { 1, 2, 3 }, QuickSort.Sort(new int[] { 1, 2, 3 }));
         assertArrayEquals(new int[] { 1, 2, 3, 4 }, QuickSort.Sort(new int[] { 1, 2, 3, 4 }));
     }
+
+    @Test
+    public void When_Quick_Sort_Is_Passed_An_Unsorted_Two_Element_Array_It_Returns_A_Sorted_Array() {
+        assertArrayEquals(new int[] { 1, 2 }, QuickSort.Sort(new int[] { 2, 1 }));
+    }
 }
