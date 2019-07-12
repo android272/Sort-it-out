8650b0118db3cb72c6a7e534130f35994728ef4c
Apullins <android2772@gmail.com>
2019-07-12 16:58:20 -0400

    Got the tests to pass.

--- test/QuickSortTest.java
+++ test/QuickSortTest.java
@@ -20,4 +20,11 @@ public class QuickSortTest {
         assertArrayEquals(new int[] { 1, 2, 3 }, QuickSort.Sort(new int[] { 3, 1, 2 }));
         assertArrayEquals(new int[] { 1, 2, 3 }, QuickSort.Sort(new int[] { 3, 2, 1 }));
     }
+
+    @Test
+    public void When_Quick_Sort_Is_Passed_An_Unsorted_Six_Element_Array_It_Returns_A_Sorted_Array() {
+        assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6 }, QuickSort.Sort(new int[] { 1, 5, 4, 6, 3, 2 }));
+        assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6 }, QuickSort.Sort(new int[] { 6, 3, 1, 5, 2, 4 }));
+        assertArrayEquals(new int[] { 1, 2, 3, 4, 5, 6 }, QuickSort.Sort(new int[] { 6, 5, 4, 3, 2, 1 }));
+    }
 }
