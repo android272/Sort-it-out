4f3c3bd379831d6c44b2920e03ffed0d6989f6eb
Apullins <android2772@gmail.com>
2019-07-15 14:52:00 -0400

    Wrote test for when Heap Sort is passed an unsorted array of length four and got it to pass.

--- test/HeapSortTest.java
+++ test/HeapSortTest.java
@@ -20,4 +20,11 @@ public class HeapSortTest {
         assertArrayEquals(new int[] { 1, 2, 3 }, HeapSort.Sort(new int[] { 2, 3, 1 }));
         assertArrayEquals(new int[] { 1, 2, 3 }, HeapSort.Sort(new int[] { 1, 3, 2 }));
     }
+
+    @Test
+    public void When_Heap_Sort_Is_Passed_An_Unsorted_Five_Element_Array_It_Returns_A_Sorted_Array() {
+        assertArrayEquals(new int[] { 1, 2, 3, 4, 5 }, HeapSort.Sort(new int[] { 5, 3, 4, 2, 1 }));
+        assertArrayEquals(new int[] { 1, 2, 3, 4, 5 }, HeapSort.Sort(new int[] { 4, 2, 5, 3, 1 }));
+        assertArrayEquals(new int[] { 1, 2, 3, 4, 5 }, HeapSort.Sort(new int[] { 1, 4, 3, 2, 5 }));
+    }
 }
